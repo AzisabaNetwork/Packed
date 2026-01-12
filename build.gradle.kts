ed1c2c4dbf6f811952b694f72a2ed615456f555a
tksimeji <tksimeji@outlook.com>
2026-01-13 07:42:43 +0900

    build: add the publishing task to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    `maven-publish`
     kotlin("jvm") version libs.versions.kotlin.get()
     kotlin("plugin.serialization") version libs.versions.kotlin.get()
 }
@@ -25,6 +26,32 @@ kotlin {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            artifact(tasks.jar)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "azisaba"
+            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://repo.azisaba.net/repository/maven-snapshots/")
+            } else {
+                uri("https://repo.azisaba.net/repository/maven-releases/")
+            }
+            credentials {
+                username = System.getenv("REPO_USERNAME")
+                password = System.getenv("REPO_PASSWORD")
+            }
+        }
+    }
 }
 
 tasks.test {
