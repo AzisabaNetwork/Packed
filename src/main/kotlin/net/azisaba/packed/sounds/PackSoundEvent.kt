32e50d1806bd5decf76b755d74193e67837c5e04
tksimeji <tksimeji@outlook.com>
2026-02-24 22:49:15 +0900

    feat: add sound event model

---
+++ src/main/kotlin/net/azisaba/packed/sounds/PackSoundEvent.kt
@@ -0,0 +1,36 @@
+package net.azisaba.packed.sounds
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.azisaba.packed.KeySerializer
+import net.kyori.adventure.key.Key
+
+@Serializable
+data class PackSoundEvent(
+    val sounds: Collection<PackSound>,
+    @Serializable(with = KeySerializer::class)
+    val subtitle: Key? = null,
+    val replace: Boolean = false,
+) {
+    @Serializable
+    data class PackSound(
+        val type: PackSoundType,
+        @Serializable(with = KeySerializer::class)
+        val name: Key,
+        val volume: Float = 1f,
+        val pitch: Float = 1f,
+        val weight: Int = 1,
+        val attenuationDistance: Int = 16,
+        val preload: Boolean = false,
+        val stream: Boolean = false,
+    )
+
+    @Serializable
+    enum class PackSoundType {
+        @SerialName("file")
+        FILE,
+
+        @SerialName("event")
+        EVENT
+    }
+}
